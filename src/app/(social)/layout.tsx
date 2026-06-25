c4244d0fef884e6de254ea4afc7a87baf129e0fe
Faiz-7716 <pmdfaiz08@gmail.com>
2026-06-25 17:40:57 +0530

    style: pushed sidebar further left by expanding layout max-width and increased center feed width

--- src/app/(social)/layout.tsx
+++ src/app/(social)/layout.tsx
@@ -10,9 +10,9 @@ export default function SocialLayout({
   return (
     <div className="flex flex-col min-h-screen pb-16 md:pb-0">
       <Navbar />
-      <div className="mx-auto max-w-[1400px] px-2 sm:px-4 lg:px-6 xl:px-8 flex justify-start lg:gap-8 w-full mt-6 flex-1">
+      <div className="mx-auto max-w-[1800px] px-1 sm:px-2 lg:px-4 flex justify-start lg:gap-4 w-full mt-6 flex-1">
         <LeftSidebar />
-        <div className="flex-1 max-w-[1000px] min-w-0 w-full mb-12">
+        <div className="flex-1 max-w-[1200px] min-w-0 w-full mb-12">
           {children}
         </div>
       </div>
